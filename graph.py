b04066f8c5ab1f633c3408fabd202ef920b65b03
Ramna Sidharta <ramna.thors@gmail.com>
2017-10-25 21:01:42 -0200

    basic operations that remainwed (methods in Graph)

--- graph.py
+++ graph.py
@@ -1,3 +1,18 @@
+"""
+It represents a Graph with the following basic operations:
+    vertex addition;
+    vertex remotion;
+    edge addition (connection between two vertices);
+    edge remotion;
+    order of graph (number of vertices);
+    to provide the vertex set;
+    to provide a specific vertex;
+    to provide adjacent vertices;
+    degree of an vertex.
+
+And the additional functions:
+
+"""
 class Graph(object):
     
     def __init__(self):
@@ -32,19 +47,39 @@ class Graph(object):
         self.__vertices[v1].discard(v2)
 
 
-    def single_vertex(self, v):  # o que significa isso?
+    def single_vertex(self, v):  # tentar um nome melhor
         if v not in self.__vertices:
-            raise VertexNotFoundException(\
-                    "Vertex not found.")
+            raise VertexNotFoundException("Vertex not found.")
         return self.__vertices[v]
 
 
+    def order(self):
+        return len(self.__vertices)
+
+
+    def vertices(self):
+        return set(self.__vertices.keys())
+
+
+    def vertex(self, something):
+        ...  # ?
+
+
+    def adjacent(self, v):
+        if v not in self.__vertices:
+            raise VertexNotFoundException("Vertex not found.")
+        return self.--vertices[v]
+
+    
+    def degree(self, v):
+        return len(self.adjacent(v))
+
 
 class VertexNotFoundException(Exception):
-    
+
     def __init__(self, message = ''):
         self.message = message
 
 
     def __str__(self):
-        repr(self.message)            
+        repr(self.message)
